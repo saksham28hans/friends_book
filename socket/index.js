35146c3e4e6c05cdd63ed7d5c95fac506a651bc2
Saksham Hans <saksham28hans@gmail.com>
2023-07-20 19:32:58 +0530

    chatfix2

--- socket/index.js
+++ socket/index.js
@@ -63,6 +63,6 @@ io.on("connection",(socket)=>{
     })
 })
 
-server.listen(PORT,()=>{
-    console.log(`Listening on port${PORT}`);
-});
+// server.listen(PORT,()=>{
+//     console.log(`Listening on port${PORT}`);
+// });
